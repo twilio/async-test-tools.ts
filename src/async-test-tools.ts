33bd1553fa8cb74af9a602676a2dfd4501fd33d5
Sergei Chertkov <schertkov@twilio.com>
2017-03-08 19:53:26 +0200

    Add Async.tick method

--- src/async-test-tools.ts
+++ src/async-test-tools.ts
@@ -21,6 +21,13 @@ class Async {
     });
   }
 
+  /**
+   * Waits for given event on EventEmitter
+   * @param {EventEmitter} emitter Instance of EventEmitter-compatible entity
+   * @param {string} eventName Name of event to wait
+   * @param {function} [condition] Event is being ignored if condition passed and returns false
+   * @returns {Promise<void>}
+   */
   static waitForEvent(emitter : any, eventName : string, condition? : Function) {
     return new Promise((resolve, reject) => {
       function handler() {
@@ -31,6 +38,16 @@ class Async {
       }
 
       emitter.on(eventName, handler);
+    });
+  }
+
+  /**
+   * Gives control to the event loop for one tick
+   * Useful to execute some asynchronous events in test
+   */
+  static async tick() {
+    return new Promise((resolve, reject) => {
+      setTimeout(resolve, 0);
     });
   }
 }
